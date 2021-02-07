fe2b98c7f2278644feee3700bf275018ef60fc5c
alirez <alisoltt00@gmail.com>
2021-02-07 09:45:20 +0330

    changed a function name

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 if(document.getElementsByClassName("im_history_messages_peer")[0] != undefined){
     var observer2 = new MutationObserver(function(mutationsList, observer2) {
-        setrtlauto();
+        setdirauto();
     });
     observer2.observe(document.getElementsByClassName("im_history_messages_peer")[0], {characterData: false, childList: true, attributes: false});
 }
@@ -8,14 +8,14 @@ if(document.getElementsByClassName("im_history_messages_peer")[0] != undefined){
 
 const observer = new MutationObserver(function(mutationsList, observer) {
     observer2 = new MutationObserver(function(mutationsList, observer2) {
-        setrtlauto();
+        setdirauto();
     });
     observer2.observe(document.getElementsByClassName("im_history_messages_peer")[0], {characterData: false, childList: true, attributes: false});
-    setrtlauto();
+    setdirauto();
 });
 observer.observe(document.getElementsByClassName("im_history_messages")[0], {characterData: false, childList: true, attributes: false});
 
-    function setrtlauto(){
+    function setdirauto(){
         [].forEach.call(document.querySelectorAll('.im_message_photo_caption,.im_message_document_caption'), function(el){
             el.setAttribute('dir','auto');
         })
